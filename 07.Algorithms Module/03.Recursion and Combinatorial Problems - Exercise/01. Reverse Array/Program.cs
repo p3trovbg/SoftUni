e1714b094458ec8e3f6a8b1984b330599668a267
p3trovbg <georgi1204g@gmail.com>
2022-05-31 22:30:01 +0300

    Update

--- 07.Algorithms Module/03.Recursion and Combinatorial Problems - Exercise/01. Reverse Array/Program.cs
+++ 07.Algorithms Module/03.Recursion and Combinatorial Problems - Exercise/01. Reverse Array/Program.cs
@@ -4,9 +4,27 @@ namespace _01._Reverse_Array
 {
     class Program
     {
+        private static string[] arr;
         static void Main(string[] args)
+        {  
+            arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            ReverseArray(0);
+        }
+
+        private static void ReverseArray(int idx)
         {
-            Console.WriteLine("Hello World!");
+          if(idx == arr.Length / 2)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            var temp = arr[idx];
+            arr[idx] = arr[arr.Length - 1 - idx];
+            arr[arr.Length - 1 - idx] = temp;
+            
+            ReverseArray(idx + 1);
         }
     }
 }
+ 
---
+++ 07.Algorithms Module/03.Recursion and Combinatorial Problems - Exercise/03. Connected Areas in Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Connected_Areas_in_Matrix
+{
+    class Program
+    {
+        private static int row;
+        private static int col;
+        private static char[,] matrix;
+        static void Main(string[] args)
+        {
+            row = int.Parse(Console.ReadLine());
+            col = int.Parse(Console.ReadLine());
+            matrix = new char[row, col];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var colElements = Console.ReadLine();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = colElements[j];
+                }
+            }
+            ;
+        }
+    }
+}
