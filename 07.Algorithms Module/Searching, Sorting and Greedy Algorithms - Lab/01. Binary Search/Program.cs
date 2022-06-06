de8a9aa3efb9206051d16f4849b1bc76a9f3e041
p3trovbg <georgi1204g@gmail.com>
2022-06-07 02:39:21 +0300

    Update

---
+++ 07.Algorithms Module/Searching, Sorting and Greedy Algorithms - Lab/01. Binary Search/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World!");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Hello, World!");
+
+}
---
+++ 07.Algorithms Module/Searching, Sorting and Greedy Algorithms - Lab/01.Binary Search/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _01.Binary_Search
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            int element = int.Parse(Console.ReadLine());
+
+            Binary_Search(input, element);
+        }
+
+        private static void Binary_Search(int[] input, int element)
+        {
+            int leftPosition = 0;
+            int rightPosition = input.Length - 1;
+
+            while (leftPosition <= rightPosition)
+            {
+                var middle = (leftPosition + rightPosition) / 2;
+                if(input[middle] == element)
+                {
+                    Console.WriteLine(middle);
+                    return;
+                }
+                else if(element > middle)
+                {
+                    leftPosition = middle + 1;
+                }
+                else
+                {
+                    rightPosition = middle - 1;
+                }
+            }
+        }
+    }
+}
