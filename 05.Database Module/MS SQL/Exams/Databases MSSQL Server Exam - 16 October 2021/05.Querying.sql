17b281689fe87a1f898ad0fb596bd7f967a09387
Georgi Petrov <georgi1204g@gmail.com>
2022-06-17 17:33:53 +0300

    Update exam exercises

--- 05.Database Module/MS SQL/Exams/Databases MSSQL Server Exam - 16 October 2021/05.Querying.sql
+++ 05.Database Module/MS SQL/Exams/Databases MSSQL Server Exam - 16 October 2021/05.Querying.sql
@@ -28,3 +28,26 @@ SELECT Id,
  ORDER BY ClientName
 
 --Problem 4
+SELECT TOP(5) 
+	CigarName,
+	PriceForSingleCigar,
+	ImageURL
+  FROM Cigars c
+   JOIN Sizes s ON s.Id = c.SizeId
+  WHERE s.[Length] >= 12 AND (c.CigarName LIKE '%ci%' OR
+        c.PriceForSingleCigar > 50) AND s.RingRange > 2.55
+  ORDER BY c.CigarName ASC, c.PriceForSingleCigar DESC
+
+--Problem 6
+SELECT 
+	CONCAT(c.FirstName, ' ', c.LastName) FullName,
+	a.Country,
+	a.ZIP,
+	CONCAT('$', MAX(cig.PriceForSingleCigar)) AS CigarPrice
+  FROM Clients c
+  JOIN Addresses a ON a.Id = c.AddressId
+  JOIN ClientsCigars cc ON cc.ClientId = c.Id
+  JOIN Cigars cig ON cig.Id = cc.CigarId
+  WHERE a.ZIP NOT LIKE '%[^0-9]%'
+  GROUP BY c.FirstName, c.LastName, a.Id, a.Country, a.ZIP
+  ORDER BY FullName
