955e1f5badfea5c5af8fdb972257d6a3c5899f82
p3trovbg <georgi1204g@gmail.com>
2022-06-22 14:54:58 +0300

    Update

--- 05.Database Module/Entity Framework Core/01.Entity Framework Introduction/01.ADO NET/Program.cs
+++ 05.Database Module/Entity Framework Core/01.Entity Framework Introduction/01.ADO NET/Program.cs
@@ -7,13 +7,68 @@ namespace _01.ADO_NET
     {
         static void Main(string[] args)
         {
-            using
-                (
-                SqlConnection conn = new SqlConnection("Server=localhost;User Id=sa;Password=Softuni!23;Database=SoftUni;"))
+            var connectionString = "Server=localhost;User Id=sa;Password=Softuni!23;Database=SoftUni;";
+            try
             {
-                conn.Open();
+                using
+               (
+               SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Successfull login!");
+
+
+                    //Employee count command
+                    string query = @"SELECT COUNT(*)
+                                       FROM Employees";
+                    var command = new SqlCommand(query, connection);
+                    var count = command.ExecuteScalar();
+                    Console.WriteLine($"Online people: {count}");
+
+
+                    //Employees information
+                    string input = String.Empty;
+                    while (input != "Close")
+                    {
+                        Console.WriteLine("Input your command");
+                        input = Console.ReadLine();
+                        //Here, we can invoke this command many times.
+                        if (input == "Give me all employees")
+                        {
+                            ReadAllEmployees(connection, out query, out command);
+                        }
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine("Wrong username or password!");
+                Console.WriteLine(ex.Number);
             }
 
+        }
+
+        private static void ReadAllEmployees(SqlConnection connection, out string query, out SqlCommand command)
+        {
+            query = @"SELECT 
+                                FirstName, 
+                                LastName,
+                                JobTitle
+                                FROM Employees";
+
+            command = new SqlCommand(query, connection);
+            var reader = command.ExecuteReader();
+            var idx = 1;
+            while (reader.Read())
+            {
+                string firstName = (string)reader.GetString(0);
+                string lastName = (string)reader.GetString(1);
+                string jobTitle = (string)reader.GetString(2);
+                Console.WriteLine("===============================================");
+                Console.WriteLine($"{idx++}.{firstName} {lastName} -> Job name: {jobTitle}");
+            }
+
+            reader.Close();
         }
     }
 }
