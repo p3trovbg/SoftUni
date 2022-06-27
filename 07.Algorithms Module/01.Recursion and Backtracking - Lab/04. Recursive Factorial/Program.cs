b1e9c28e90ad201436d323ca9c6eddf30f35cd8f
p3trovbg <georgi1204g@gmail.com>
2022-06-27 22:26:23 +0300

    Update

---
+++ 07.Algorithms Module/01.Recursion and Backtracking - Lab/04. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SumFactorial(input));
+        }
+
+        private static int SumFactorial(int result)
+        {
+            if(result == 0)
+            {
+                return 1;
+            }
+
+            return result * SumFactorial(result - 1);
+        }
+    }
+}
