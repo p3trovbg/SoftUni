f06d3f2022a3d869beee3344cf7bb775d7d147e4
p3trovbg <georgi1204g@gmail.com>
2022-07-10 23:40:26 +0300

    Update

--- 07.Algorithms Module/Resolve - same exercises/Recursion and Combinatorial Problems - Exercise/05. SchoolTeams/Program.cs
+++ 07.Algorithms Module/Resolve - same exercises/Recursion and Combinatorial Problems - Exercise/05. SchoolTeams/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._SchoolTeams
 {
@@ -6,7 +8,32 @@ namespace _05._SchoolTeams
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PetOwner[] petOwners =
+       { new PetOwner { Name="Higa, Sidney",
+              Pets = new List<string>{ "Scruffy", "Sam", "Gosho" } },
+          new PetOwner { Name="Ashkenazi, Ronen",
+              Pets = new List<string>{ "Walker", "Sugar", "Bobcho" } },
+          new PetOwner { Name="Price, Vernette",
+              Pets = new List<string>{ "Scratches", "Diesel", "Minko" } } };
+
+            // Query using SelectMany().
+            IEnumerable<string> query1 = petOwners.SelectMany(petOwner => petOwner.Pets);
+
+            // Only one foreach loop is required to iterate
+            // through the results since it is a
+            // one-dimensional collection.
+            foreach (string pet in query1)
+            {
+                Console.WriteLine(pet);
+            }
         }
     }
 }
+
+
+class PetOwner
+{
+    public string Name { get; set; }
+    public List<String> Pets { get; set; }
+}
+
