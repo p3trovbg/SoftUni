99339a4f2c029063740efe523c2636623d1e61de
p3trovbg <georgi1204g@gmail.com>
2022-07-12 23:37:50 +0300

    Add sorting algorithms exercises

---
+++ 07.Algorithms Module/Resolve - same exercises/Searching, Sorting and Greedy Algorithms - Lab/SortingAlgortihms/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingAlgortihms
+{
+    public class Program
+    {
+        private static int[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+
+            //Selection sort -> SelectionSort(elements);
+            BubbleSort(elements);
+            Console.WriteLine(String.Join(" ", elements));
+        }
+
+        private static void BubbleSort(int[] elements)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SelectionSort(int[] elements)
+        {
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                var minIdx = i;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    var minElement = elements[minIdx];
+                    var nextElement = elements[j];
+
+                    if (minElement > nextElement)
+                    {
+                        minIdx = j;
+                    }
+                }
+
+                Swap(i, minIdx);
+            }
+        }
+
+        private static void Swap(int firstIdx, int minIdx)
+        {
+            var temp = elements[minIdx];
+            elements[minIdx] = elements[firstIdx];
+            elements[firstIdx] = temp;
+        }
+    }
+}
