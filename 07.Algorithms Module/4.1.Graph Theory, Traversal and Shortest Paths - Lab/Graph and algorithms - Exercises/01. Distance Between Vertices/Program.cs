4713a69e6989f134584cca0e07d1e564e0b57203
p3trovbg <georgi1204g@gmail.com>
2022-07-15 23:58:40 +0300

    Update

---
+++ 07.Algorithms Module/4.1.Graph Theory, Traversal and Shortest Paths - Lab/Graph and algorithms - Exercises/01. Distance Between Vertices/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Distance_Between_Vertices
+{
+    internal class Program
+    {
+        private static HashSet<int>[] graph;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int pathCount = int.Parse(Console.ReadLine());
+
+            graph = new HashSet<int>[n];
+            FillGraph(n);
+        }
+
+        private static void FillGraph(int n)
+        {
+            //2 - edges
+            //2 - relations 
+            //1:2
+            //2:
+            //1 - 2
+            //2 - 1
+
+            for (int node = 0; node < n; node++)
+            {
+
+            }
+        }
+    }
+}
