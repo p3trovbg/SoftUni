8a57c17428130052c8a84e27d3aa98a3a899a1d8
p3trovbg <georgi1204g@gmail.com>
2022-07-23 12:04:39 +0300

    Update

---
+++ 07.Algorithms Module/6.1 Dynamic Programming - Exercises/Dynamic Programming - Exercise/01. Binomial Coefficients/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Binomial_Coefficients
+{
+    internal class Program
+    {
+        private static Dictionary<string, long> coeffiecients;
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+            coeffiecients = new Dictionary<string, long>();
+
+            var result = GetBinomialCoefficient(row, col);
+            Console.WriteLine(result);
+            ;
+        }
+
+        private static long GetBinomialCoefficient(int row, int col)
+        {
+            var coefficient = $"{row} {col}";
+            if(coeffiecients.ContainsKey(coefficient))
+            {
+                return coeffiecients[coefficient];
+            }
+            if (col == 0 || col == row || row <= 1)
+            {
+                return 1;
+            }
+
+            var result =  GetBinomialCoefficient(row - 1, col) + GetBinomialCoefficient(row - 1, col - 1);
+
+            coeffiecients[coefficient] = result;
+
+            return result;
+        }
+    }
+}
