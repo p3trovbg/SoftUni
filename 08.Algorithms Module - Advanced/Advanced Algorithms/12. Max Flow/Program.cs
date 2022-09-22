3829af5b1ed4e9350ae89af4b8e72ba3e2255d8f
p3trovbg <georgi1204g@gmail.com>
2022-09-22 13:28:27 +0300

    Add strongly connected components with kosaraju's algorithm

---
+++ 08.Algorithms Module - Advanced/Advanced Algorithms/12. Max Flow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12._Max_Flow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
