d727898a75d6ee56361c0c7a131a08b91a69977d
George <p3trovsit@gmail.com>
2022-09-30 15:28:47 +0300

    Add Longest increasing subsequence problem

---
+++ 08.Algorithms Module - Advanced/Advanced Algorithms/17. Longest Increasing Subsequence/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _17._Longest_Increasing_Subsequence
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            var len = new int[numbers.Length];
+            var prev = new int[numbers.Length];
+            var bestLength = 0;
+            var bestIdx = 0;
+
+            for (int currentElementIdx = 0; currentElementIdx < numbers.Length; currentElementIdx++)
+            {
+                var currentElement = numbers[currentElementIdx];
+                
+                var currentLength = 1;
+                var currentPrev = -1;
+                
+
+                for (int prevIdx = currentElementIdx - 1; prevIdx >= 0; prevIdx--)
+                {
+                    var previousElement = numbers[prevIdx];
+                    var previousLength = len[prevIdx];
+
+                    if (currentElement > previousElement &&
+                       currentLength <= previousLength + 1)
+                    {
+                        currentLength = previousLength + 1;
+                        currentPrev = prevIdx;
+                    }
+                }
+
+                len[currentElementIdx] = currentLength;
+                prev[currentElementIdx] = currentPrev; 
+
+                if(currentLength > bestLength)
+                {
+                    bestLength = currentLength;
+                    bestIdx = currentElementIdx;
+                }
+            }
+
+            var path = new Stack<int>();
+
+            while (bestIdx != -1)
+            {
+                path.Push(numbers[bestIdx]); ;
+                bestIdx = prev[bestIdx];
+            }
+
+            Console.WriteLine(String.Join(" ", path));
+        }
+    }
+}
